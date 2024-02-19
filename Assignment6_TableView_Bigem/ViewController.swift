5fec4aa2af04657767d52f170785b8ce86ec16fb
Bigem Maharjan <bigemmaharjan520@gmail.com>
2024-02-18 16:07:38 -0800

    Initial Commit

---
+++ Assignment6_TableView_Bigem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment6_TableView_Bigem
+//
+//  Created by user240741 on 2/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
